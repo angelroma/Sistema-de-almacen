28bb0392917a2a8fe01bee67d7dbe55ef0d7411d
Angel Rodriguez <angelr10mar@gmail.com>
2020-05-13 16:59:07 -0500

    Add navigation bar with simple style

---
+++ portal-client/src/components/Navigation.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav>
+    <router-link to="/productos">Productos</router-link>
+    <router-link to="/reportes">Reportes</router-link>
+    <router-link to="/inventario">Inventario</router-link>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+nav {
+  display: flex;
+  background-color: #5f5f5f;
+}
+
+a {
+  padding: 10px;
+  text-decoration: none;
+  color: #fff;
+  font-size: 20px;
+
+  &:hover {
+      background-color: #333333;
+  }
+}
+</style>
--- portal-client/src/router/index.js
+++ portal-client/src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Products from '../views/Products.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/productos',
+    name: 'Productos',
+    component: Products
   }
 ]
 
